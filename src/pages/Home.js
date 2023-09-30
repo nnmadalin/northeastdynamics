f78cf9b22f8718b6514aacabf96883ffd4e652d9
nnmadalin <neauna.madalin2006@gmail.com>
2023-09-30 15:04:05 +0300

    add links and images sponsors

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,11 @@ import CardResults from '.././layouts/card_results.js'
 import CardSponsors from '.././layouts/card_sponsors.js'
 
 import video from '.././assets/video/Robot Reveal _ 2021.mp4'
-import NED from '.././assets/images/NED.jpg'
+import about from '.././assets/images/about.jpg'
+
+import national_championship_2023 from '.././assets/images/national_championship-2023.JPG'
+import national_championship_2021 from '.././assets/images/national_championship-2021.jpg'
+import regional_2023 from '.././assets/images/regional-2023.jpg'
 
 
 import consiliul_local from '.././assets/sponsors/clDorohoi.png'
@@ -50,7 +54,7 @@ function Home() {
           <p>We started on this path since 2018. Every year we evolve and see that "Sky is not the limit". We make robots, but it's not enough. We want to make history! Season 8 brings us new challenges, but we're ready to give it our best shot and capitalize on Gracious Professionalism. Gracious team, Gracious work, Gracious minds. The Centerstage Challenge has arrived at our door - we're ready to give it our best (again). Welcome to our website! This is us.</p>
         </div>
         <div className='right'>
-          <img src={NED} />
+          <img src={about} />
         </div>
       </div>
 
@@ -61,21 +65,21 @@ function Home() {
         
         <div className='cards'>
           <CardResults 
-            image_url={NED} 
+            image_url={national_championship_2023} 
             position="l" 
             title="National Championship 2023" 
             description="20th place out of 160"
           />
 
           <CardResults 
-            image_url={NED} 
+            image_url={regional_2023} 
             position="r" 
             title="Regional robotics 2023" 
             description="Control Award 2nd place"
           />
 
           <CardResults 
-            image_url={NED} 
+            image_url={national_championship_2021} 
             position="l" 
             title="National Championship 2021" 
             description="Winning Alliance 3rd place, Innovate Award 3rd place"
@@ -94,6 +98,7 @@ function Home() {
         <div className='cards'>
           <CardSponsors 
             image_url={consiliul_local}
+            redirect_url="https://www.primariadorohoi.ro/"
           />
           <CardSponsors 
             image_url={dorohoinews}
@@ -101,25 +106,37 @@ function Home() {
           />
           <CardSponsors 
             image_url={soofa}
+            redirect_url="https://www.sweetony.ro/"
           />
           <CardSponsors 
             image_url={magistra}
+            redirect_url="https://www.facebook.com/farmaciamagistra/?locale=ro_RO"
           />
           <CardSponsors 
             image_url={symm}
+            redirect_url="https://www.symmetrica.ro/"
           />
           <CardSponsors 
             image_url={miracom}
+            redirect_url="https://www.miracom.eu/"
           />
           <CardSponsors 
             image_url={cucorani}
+            redirect_url="https://www.popascucorani.ro/"
           />
         </div>
       </div>
 
       {/* SOCIAL MEDIA */}
 
-      
+      <div className='social'>
+        <h1>The lates posts on our social relays</h1>
+
+        <div className='cards'>
+
+        </div>
+      </div>
+
     </>
   );
 }
